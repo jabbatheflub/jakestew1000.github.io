6df1da9421638089b337bc32f64a6ca08232e537
jakestew1000 <jakestew@hotmail.co.uk>
2019-07-03 18:48:57 +0100

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,12 @@
+    	var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+function multiply(num1,num2) {
+  var result = num1 * num2;
+  return result;
+}
+multiply(4, 7);
+multiply(20, 20);
+multiply(0.5, 3);
+
+alert(result);
